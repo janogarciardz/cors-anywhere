f2af70cae9b8cbbea375971d0d1b8592db37c453
Alejandro Garcia <janogarciardz@gmail.com>
2023-10-25 00:12:31 -0600

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3030;
+const axios = require("axios");
+
+app.get("/", (req, res) => {
+  const { location, radius, key } = req.query;
+
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&key=${key}`
+    )
+    .then((response) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.listen(3030);
